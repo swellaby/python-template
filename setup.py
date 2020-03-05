a0e34ca68d1238a54a3b053ccdc7e0bc711f1f3d
Caleb Cartwright <caleb.cartwright@outlook.com>
2020-03-05 12:00:00 +0000

    meta: add install_requires

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("requirements.txt", "r") as fh:
+    install_requires = fh.read()
+
 version = {}
 exec(open("/version.py").read(), version)
 
@@ -17,6 +20,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/swellaby/",
     packages=find_packages(),
+    install_requires=install_requires,
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Programming Language :: Python :: 3 :: Only",
